f219b65da4d181edcdb7af69cfcd76e7f908b083
Joy Chela <joychela000@gmail.com>
2025-04-27 09:20:00 +0300

    setting up the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import ClientList from './components/clientList.jsx';
+import ClientDetail from './components/ClientDetails.jsx';
+import ClientForm from './components/ClientForm';
+import ProgramList from './components/ProgramList';
+import ProgramForm from './components/ProgramForm';
+import Navbar from './components/Navbar';
+import './App.css';
 
 function App() {
 
   return (
-    <>
-    </>
-  )
+    <Router>
+      <div className="app">
+        <Navbar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/clients" element={<ClientList />} />
+            <Route path="/clients/new" element={<ClientForm />} />
+            <Route path="/clients/:id" element={<ClientDetail />} />
+            <Route path="/programs" element={<ProgramList />} />
+            <Route path="/programs/new" element={<ProgramForm />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
